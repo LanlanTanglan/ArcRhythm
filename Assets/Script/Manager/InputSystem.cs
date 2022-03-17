2baccf01caa36e8491b26f4c6d8fed89c03ec5dc
懒懒瑭岚 <107575574@qq.com>
2022-03-17 19:21:54 +0800

    文件架构变化

--- Assets/Script/Manager/InputSystem.cs
+++ Assets/Script/Manager/InputSystem.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
-using ArcM2;
+using ArcRhythm;
 using Util;
 
 
 
-public class PCInputManager : Singleton<PCInputManager>
+public class KeyboardInputManager : Singleton<KeyboardInputManager>
 {
     public bool isStopGame = false;
-
+    
     override protected void OnAwake()
     {
         //注册事件
@@ -20,33 +20,60 @@ public class PCInputManager : Singleton<PCInputManager>
         this.isStopGame = key;
     }
 
+    void Update()
+    {
+        if (!isStopGame)
+        {
+            
+        }
+    }
+
     //初始化
     public void Init()
     {
-        
+
     }
 }
 
 
 public class AndroidInputManager : Singleton<AndroidInputManager>
 {
+    public bool isStopGame = false;
     override protected void OnAwake()
     {
+        //注册事件
+        Singleton<GameProcessManager>.Instance.StopGameEvent += StopGame;
+    }
+
+    //暂停游戏
+    private void StopGame(bool key)
+    {
+        this.isStopGame = key;
     }
 
     public void Init()
     {
-        
+
     }
+
 }
 
 public class IOSInputManager : Singleton<IOSInputManager>
 {
+    public bool isStopGame = false;
     override protected void OnAwake()
     {
+        //注册事件
+        Singleton<GameProcessManager>.Instance.StopGameEvent += StopGame;
+    }
+
+    //暂停游戏
+    private void StopGame(bool key)
+    {
+        this.isStopGame = key;
     }
     public void Init()
     {
-        
+
     }
 }
