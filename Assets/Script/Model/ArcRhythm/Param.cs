cb4735513f27cc568d21ae19de1448f03ffbe2bd
懒懒瑭岚 <107575574@qq.com>
2022-03-19 22:13:40 +0800

    参数静态类

---
+++ Assets/Script/Model/ArcRhythm/Param.cs
@@ -0,0 +1,13 @@
+namespace ArcRhythm
+{
+    /// <summary>
+    /// 路径集合
+    /// 某人路径要么是直接带文件
+    /// 要么是父级目录(即最后一个字母一定是 / )
+    /// </summary>
+    public static class ArcPath
+    {
+        public static string prefebDirOfOperator = "Prefab/Operator/";//干员预制体地址
+        public static string prefebDirOfEnemy = "Prefab/Enemy/";//干员预制体地址
+    }
+}
