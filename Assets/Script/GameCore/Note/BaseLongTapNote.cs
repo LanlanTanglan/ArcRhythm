5d933ca47b35fcb8afb70e8f3c72390e1d67fa30
懒懒瑭岚 <107575574@qq.com>
2022-03-20 20:57:09 +0800

    铺面管理器

---
+++ Assets/Script/GameCore/Note/BaseLongTapNote.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using ArcRhythm;
+using Util;
+
+public class BaseLongTapNote : MonoBehaviour
+{
+    public Note note;
+    public bool isStopGame = false;
+
+    void Awake()
+    {
+        //注册事件
+        Singleton<GameProcessManager>.Instance.StopGameEvent += StopGame;
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    #region 事件注册块
+    //暂停游戏
+    private void StopGame(bool key)
+    {
+        this.isStopGame = key;
+    }
+
+    #endregion
+
+    public void Init(Note n)
+    {
+        this.note = n;
+    }
+}
