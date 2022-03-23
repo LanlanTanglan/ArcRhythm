6efb29625c5741d35cbf5e0cd191063973fb1c02
懒懒瑭岚 <107575574@qq.com>
2022-03-23 19:26:07 +0800

    状态机框架更换

--- Assets/Script/Model/ArcFMS/Interface/ICondition.cs
+++ Assets/Script/Model/ArcFMS/Interface/ICondition.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-namespace ArcFMS
+namespace ArcState
 {
-    public interface ICondition<T>
+    interface ICondition
     {
 
     }
