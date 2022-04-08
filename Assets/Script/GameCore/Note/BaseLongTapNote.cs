ff591e3439a732f7eac354fffd60e08b078abf50
懒懒瑭岚 <107575574@qq.com>
2022-04-08 20:14:00 +0800

    修改判定点贴图

--- Assets/Script/GameCore/Note/BaseLongTapNote.cs
+++ Assets/Script/GameCore/Note/BaseLongTapNote.cs
@@ -68,7 +68,6 @@ public class BaseLongTapNote : BaseNote
                 {
                     Destroy(this.gameObject);
                 }
-                //TODO
             }
         }
     }
