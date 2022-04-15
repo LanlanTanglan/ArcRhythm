60df85ad4e34b74d1aee7e1c7bd009f00c035732
懒懒瑭岚 <107575574@qq.com>
2022-04-15 21:26:43 +0800

    UI界面测试

---
+++ Assets/Script/GameCore/ArcUI/ChapterPanel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterPanel : BasePanel
+{
+    void Awake()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+
+}
---
+++ Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using ArcRhythm;
+using Util;
+
+public class UIManager : Singleton<UIManager>
+{
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+        //注册界面, 并给界面绑定脚本
+
+    }
+}
