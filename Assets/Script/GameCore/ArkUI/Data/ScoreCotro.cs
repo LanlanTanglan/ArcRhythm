16c0beac86142b783016e89834b01c4aa65d8956
懒懒瑭岚 <107575574@qq.com>
2022-04-23 23:00:39 +0800

    UI代码

---
+++ Assets/Script/GameCore/ArkUI/Data/ScoreCotro.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// 用于控制分数
+/// </summary>
+public class ScoreCotro : MonoBehaviour
+{
+    public TextMeshPro score;
+    void Awake()
+    {
+        score = GetComponent<TextMeshPro>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SetScore()
+    {
+        
+    }
+}
