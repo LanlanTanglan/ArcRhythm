4f40302d50ae09df5dca51accfc9747ad084c695
懒懒瑭岚 <107575574@qq.com>
2022-05-26 21:33:02 +0800

    次态

--- Assets/Script/Lib/ArkStateMachine/State.cs
+++ Assets/Script/Lib/ArkStateMachine/State.cs
@@ -15,6 +15,7 @@ public abstract class State<T>
     /// 连接次态
     /// 连接次态
     /// 连接次态
+    /// 连接次态
     /// </summary>
     public Dictionary<Condition<T>, State<T>> nextStates = new Dictionary<Condition<T>, State<T>>();
 
